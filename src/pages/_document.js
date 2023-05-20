581925a056df08c3fe76713fc6b8ecf9d6b5bc11
CrackAlejo <ricardonicolasalejo@gmail.com>
2023-05-20 17:16:32 -0300

    App version 1.8

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document';
- 
+
+import Script from 'next/script';
+
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
       </Head>
       <body>
-        <Main />
+        <Main>
+          <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>
+          <Script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></Script>
+        </Main>
         <NextScript />
       </body>
     </Html>
